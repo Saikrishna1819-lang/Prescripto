ddc4dc0f41e1d87fb9c4963d17a7dd00a9daf4bd
Saikrishna1819-lang <krishnasai69219@gmail.com>
2025-06-26 23:53:05 +0530

    created all doctors page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     const [token, setToken]=useState(true)
   return (
     <div className='flex justify-between items-center text-sm py-4 mb-5 border-b border-b-gray-400'>
-        <img className='w-44 cursor-pointer' src={assets.logo} alt=""></img>
+        <img onClick={()=>{navigate("/")}} className='w-44 cursor-pointer' src={assets.logo} alt=""></img>
         <ul className='hidden md:flex items-start gap-5 font-medium '>
             <NavLink to='/'>
                 <li className="py-1">HOME</li>
@@ -36,7 +36,7 @@ const NavBar = () => {
                     <img className='w-2.5 ' src={assets.dropdown_icon}></img>
                     <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                          <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                            <p onClick={()=>{navigate("my-profile")}}  onClickclassName="hover:text-black cursor-pointer">My Profile</p>
+                            <p onClick={()=>{navigate("my-profile")}}  className="hover:text-black cursor-pointer">My Profile</p>
                             <p onClick={()=>{navigate("my-appointments")}} className="hover:text-black cursor-pointer">My Appointments</p>
                             <p onClick={()=>{setToken(false)}} className="hover:text-black cursor-pointer">Logout</p>
                          </div> 
