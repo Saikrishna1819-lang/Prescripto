aa0d7c65a52e4b978f5516a82d516a79d2d9a43c
Saikrishna1819-lang <krishnasai69219@gmail.com>
2025-07-24 18:13:47 +0530

    creating cancel-appointment api in admin panel and also creating all appointments interface

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,74 +1,89 @@
 import { createContext, useEffect, useState } from "react";
- import axios from "axios";
- import { toast } from "react-toastify";
+import axios from "axios";
+import { toast } from "react-toastify";
 
+export const AppContext = createContext();
 
-export const AppContext=createContext()
-const AppContextProvider=(props)=>{
-    const currencySymbol='$'
-     const backendUrl=import.meta.env.VITE_BACKEND_URL
-    const [doctors,setDoctors]=useState([])
-    const [userData,setUserData]=useState(false)
-    const [token,setToken]=useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
+const AppContextProvider = (props) => {
+  const currencySymbol = '$';
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
+  const [doctors, setDoctors] = useState([]);
+  const [userData, setUserData] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem('token') || false);
 
-   
-    const getDoctorsData=async()=>{
-       try{
-         const {data}=await axios.get(backendUrl+'/api/doctor/list')
-         if(data.success){
-            setDoctors(data.doctors)
-         }
-         else{
-            toast.error(data.message)
-         }
-       }
-       catch(error){
-        toast.error(error.message)
+  const calculateAge = (dob) => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    return age;
+  };
 
-       }
+  const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split('_');
+    return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2];
+  };
 
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + '/api/doctor/list');
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
-    const loadUserProfileData=async()=>{
-        try{
-            const {data}=await axios.get(backendUrl+'/api/user/get-profile',{headers:{token}})
-        if(data.success){
-            setUserData(data.userData)
-        }
-        else{
-            toast.error(data.message)
-            
-        }
-        }catch(error){
-            toast.error(error.message)
-        }
-    }
-     const value={
-        doctors,
-        getDoctorsData,
-        currencySymbol,
-        token,
-        setToken,
-        backendUrl,
-        userData,setUserData,
-        loadUserProfileData
-    }
-    useEffect(()=>{
-        getDoctorsData()
-    },[])
-    useEffect(()=>{
-        if(token){
-            loadUserProfileData()
-        }
-        else{
-            setUserData(false)
-        }
-    },[token])
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  };
 
-export default AppContextProvider
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + '/api/user/get-profile', {
+        headers: { token },
+      });
+      if (data.success) {
+        setUserData(data.userData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData();
+    } else {
+      setUserData(false);
+    }
+  }, [token]);
+
+  const value = {
+    doctors,
+    getDoctorsData,
+    currencySymbol,
+    token,
+    setToken,
+    backendUrl,
+    userData,
+    setUserData,
+    loadUserProfileData,
+    calculateAge,
+    slotDateFormat,
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
